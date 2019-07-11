b7617899afa95c1174be421a8782019f3293485a
LucNguyen <matkinhig@gmail.com>
2019-07-11 19:52:03 +0700

    update ListView from Firebase

---
+++ app/src/main/java/com/example/googleservicefoody/View/DangNhapActivity.java
@@ -0,0 +1,16 @@
+package com.example.googleservicefoody.View;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.googleservicefoody.R;
+
+public class DangNhapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dangnhap);
+    }
+}
--- app/src/main/java/com/example/googleservicefoody/View/SplashScreenActivity.java
+++ app/src/main/java/com/example/googleservicefoody/View/SplashScreenActivity.java
@@ -1,12 +1,21 @@
 package com.example.googleservicefoody.View;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.googleservicefoody.R;
 
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+
+    TextView txtVersion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +35,21 @@ public class SplashScreenActivity extends AppCompatActivity {
     }
 
     private void addControl() {
+        txtVersion = findViewById(R.id.txtVersion);
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(),0);
+            txtVersion.setText("Version " + packageInfo.versionName);
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent iLogin = new Intent(SplashScreenActivity.this,DangNhapActivity.class);
+                    startActivity(iLogin);
+                }
+            },2000);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
 
