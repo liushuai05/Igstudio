8c473a3883d186a3fe5a1e0f13d9e1a36fce566b
LucNguyen <matkinhig@gmail.com>
2019-07-12 18:36:52 +0700

    Login with Facebook

--- app/src/main/java/com/example/googleservicefoody/View/DangNhapActivity.java
+++ app/src/main/java/com/example/googleservicefoody/View/DangNhapActivity.java
@@ -1,9 +1,6 @@
 package com.example.googleservicefoody.View;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.res.AssetManager;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,14 +12,12 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.googleservicefoody.R;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
+import com.facebook.CallbackManager;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -33,16 +28,14 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 
-public class DangNhapActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, FirebaseAuth.AuthStateListener {
+public class DangNhapActivity extends AppCompatActivity implements FirebaseAuth.AuthStateListener, GoogleApiClient.OnConnectionFailedListener {
 
 
     TextView txtDangKy;
-    Button btnGoogle;
+    Button btnGoogle, btnFacebook;
+    CallbackManager callbackManager;
     GoogleApiClient googleApiClient;
     FirebaseAuth firebaseAuth;
-
-    public static int KIEMTRA_PROVIDER_LOGIN = 1;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,26 +50,29 @@ public class DangNhapActivity extends AppCompatActivity implements GoogleApiClie
     private void addControl() {
         txtDangKy = findViewById(R.id.txtDangKy);
         btnGoogle = findViewById(R.id.btnGoogle);
+        callbackManager= CallbackManager.Factory.create();
+        btnFacebook = (LoginButton) findViewById(R.id.btnFacebook);
         firebaseAuth = FirebaseAuth.getInstance();
-        taoClientDangNhapGoogle();
-    }
+        firebaseAuth.signOut();
 
-    private void taoClientDangNhapGoogle() {
-        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder()
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
+
         googleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this, this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
                 .build();
+
     }
 
     private void addEvent() {
         btnGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                signIn(view);
+                Intent iDangNhapGoogle = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(iDangNhapGoogle, 20);
             }
         });
 
@@ -87,51 +83,38 @@ public class DangNhapActivity extends AppCompatActivity implements GoogleApiClie
                 startActivity(intenDangKy);
             }
         });
-
     }
 
-
-    private void signIn(View view) {
-        int id = view.getId();
-        switch (id) {
-            case R.id.btnGoogle:
-                dangNhapGoogle(googleApiClient);
-                break;
-        }
-    }
-
-
-    private void dangNhapGoogle(GoogleApiClient googleApiClient) {
-        KIEMTRA_PROVIDER_LOGIN = 1;
-        Intent iDangNhapGoogle = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
-        startActivityForResult(iDangNhapGoogle, 20);
-    }
-    
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 20 && resultCode == RESULT_OK) {
             GoogleSignInResult googleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            GoogleSignInAccount googleSignInAccount = googleSignInResult.getSignInAccount();
-            String tokenID = googleSignInAccount.getIdToken();
+            final String tokenID = googleSignInResult.getSignInAccount().getIdToken();
+            AuthCredential authCredential = GoogleAuthProvider.getCredential(tokenID,"");
+            firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    Toast.makeText(DangNhapActivity.this,"Tokenid cuar ban la " + tokenID, Toast.LENGTH_LONG).show();
+                }
+            });
             chungThucDangNhap(tokenID);
         }
     }
 
 
     private void chungThucDangNhap(String tokenID) {
-        if (KIEMTRA_PROVIDER_LOGIN == 1) {
-            AuthCredential authCredential = GoogleAuthProvider.getCredential(tokenID, null);
-            firebaseAuth.signInWithCredential(authCredential);
-        }
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(tokenID, null);
+        firebaseAuth.signInWithCredential(authCredential);
     }
 
     @Override
     public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-        if (firebaseUser != null) {
+        firebaseAuth = this.firebaseAuth;
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
             Toast.makeText(this,
-                    "Dang nhap thanh cong " + firebaseUser,
+                    "Dang nhap thanh cong " + user,
                     Toast.LENGTH_LONG).show();
         } else Toast.makeText(this,
                 "Dang nhap that bai",
